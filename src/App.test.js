b18fb23293d5687b9eef96b643650672ef0c60f7
Ram Manthry <ram.manthry@gmail.com>
2022-12-06 20:06:12 +1300

    wrote a failing test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('this is designed to fail', () => {
+  expect(true).toBe(false)
+})
